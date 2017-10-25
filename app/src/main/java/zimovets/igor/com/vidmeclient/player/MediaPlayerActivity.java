de32269251220344172be841c370021b54d1c242
zimov-igor <igorzim30@gmail.com>
2017-10-25 20:46:44 +0700

    before clean2

---
+++ app/src/main/java/zimovets/igor/com/vidmeclient/player/MediaPlayerActivity.java
@@ -0,0 +1,136 @@
+package zimovets.igor.com.vidmeclient.player;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
+import android.util.Log;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import zimovets.igor.com.vidmeclient.R;
+
+public class MediaPlayerActivity extends AppCompatActivity {
+
+
+    private VideoView videoView;
+    private int position = 0;
+    private MediaController mediaController;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+
+
+        videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVisibility(View.VISIBLE);
+
+
+
+
+        // Set the media controller buttons
+        if (mediaController == null) {
+            mediaController = new MediaController(MediaPlayerActivity.this);
+
+            // Set the videoView that acts as the anchor for the MediaController.
+            mediaController.setAnchorView(videoView);
+
+
+            // Set MediaController for VideoView
+            videoView.setMediaController(mediaController);
+
+        }
+
+
+        try {
+            // ID of video file.
+            //int id = this.getRawResIdByName("myvideo");
+
+            String videoUrl="https://api.vid.me/video/18175470/stream?format=dash"; // dash
+            videoUrl = "http://www.youtubemaza.com/files/data/366/Tom%20And%20Jerry%20055%20Casanova%20Cat%20(1951).mp4";
+            Uri video = Uri.parse(videoUrl);
+            videoView.setVideoURI(video);
+
+            //videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + id));
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        videoView.requestFocus();
+
+
+        // When the video file ready for playback.
+        videoView.setOnPreparedListener(new OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mediaPlayer) {
+
+
+                videoView.seekTo(position);
+                if (position == 0) {
+                    videoView.start();
+                }
+
+                // When video Screen change size.
+                mediaPlayer.setOnVideoSizeChangedListener(new MediaPlayer.OnVideoSizeChangedListener() {
+                    @Override
+                    public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+
+                        // Re-Set the videoView that acts as the anchor for the MediaController
+                        mediaController.setAnchorView(videoView);
+                    }
+                });
+            }
+        });
+
+    }
+
+    // Find ID corresponding to the name of the resource (in the directory raw).
+    public int getRawResIdByName(String resName) {
+        String pkgName = this.getPackageName();
+        // Return 0 if not found.
+        int resID = this.getResources().getIdentifier(resName, "raw", pkgName);
+        Log.i("AndroidVideoView", "Res Name: " + resName + "==> Res ID = " + resID);
+        return resID;
+    }
+
+
+    // When you change direction of phone, this method will be called.
+    // It store the state of video (Current position)
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        // Store current position.
+        savedInstanceState.putInt("CurrentPosition", videoView.getCurrentPosition());
+        videoView.pause();
+    }
+
+
+    // After rotating the phone. This method is called.
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Get saved position.
+        position = savedInstanceState.getInt("CurrentPosition");
+        videoView.seekTo(position);
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        videoView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+}
